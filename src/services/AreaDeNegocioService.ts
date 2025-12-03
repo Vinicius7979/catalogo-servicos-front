97aea252a185aeda7d0197e960f8962589b6ebf6
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-03 16:41:56 -0400

    service de Area de Negocio

---
+++ src/services/AreaDeNegocioService.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { useApiStore } from '@/stores/api'
+
+export const AreaDeNegocioService = {
+  listar() {
+    const { getUrl } = useApiStore()
+    return axios.get(getUrl('/linguagens'))
+  },
+  salvar(dados: object) {
+    const { getUrl } = useApiStore()
+    return axios.post(getUrl('/linguagens'), dados)
+  },
+  buscarPorUuid(uuid: string) {
+    const { getUrl } = useApiStore()
+    return axios.get(getUrl(`/linguagens/${uuid}`))
+  },
+  atualizar(uuid: string, dados: object) {
+    const { getUrl } = useApiStore()
+    return axios.put(getUrl(`/linguagens/${uuid}`), dados)
+  },
+  deletar(uuid: string) {
+    const { getUrl } = useApiStore()
+    return axios.delete(getUrl(`/linguagens/${uuid}`))
+  }
+}
