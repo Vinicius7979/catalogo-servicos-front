ec06b915ca472e7b3bdfd3d68d977f7bc26e266e
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-04 14:46:29 -0400

    corpo do template

--- src/components/Area de Negocio/AreaDeNegocioFormEdit.vue
+++ src/components/Area de Negocio/AreaDeNegocioFormEdit.vue
@@ -41,44 +41,44 @@ function fechar(){
 
 <template>
     <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div class="bg-white rounded-2xl shadow-2xl p-6 w-[90%] max-w-md">
-        <h2 class="text-2xl font-bold text-center mb-4 text-gray-800">
-          Editar Área de Negócio
-        </h2>
-        <div class="flex flex-col gap-4">
-          <label class="block text-sm font-medium text-neutral-700 mb-2">área de Negócio</label>
-          <input 
-            v-model="descricao" 
-            type="text" 
-            class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-            required 
-          />
+        <div class="bg-white rounded-2xl shadow-2xl p-6 w-[90%] max-w-md">
+            <h2 class="text-2xl font-bold text-center mb-4 text-gray-800">
+                Editar Área de Negócio
+            </h2>
+            <div class="flex flex-col gap-4">
+                <label class="block text-sm font-medium text-neutral-700 mb-2">área de Negócio</label>
+                <input 
+                    v-model="descricao" 
+                    type="text" 
+                    class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    required 
+                />
 
-          <label class="block text-sm font-medium text-neutral-700 mb-2">Impacto</label>
-          <input 
-            v-model="impacto" 
-            type="text" 
-            class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-            required 
-          />
+                <label class="block text-sm font-medium text-neutral-700 mb-2">Impacto</label>
+                <input 
+                    v-model="impacto" 
+                    type="text" 
+                    class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    required 
+                />
+            </div>
+
+            <div class="flex justify-end mt-6 gap-3">
+                <button
+                    @click="fechar"
+                    class="px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-400 transition"
+                >
+                    Cancelar
+                </button>
+
+                <button 
+                    @click="salvarEdicao"
+                    class="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 transition"
+                >
+                    Salvar
+                </button>
+            </div>  
         </div>
-
-        <div class="flex justify-end mt-6 gap-3">
-          <button
-            @click="fechar"
-            class="px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-400 transition"
-          >
-            Cancelar
-          </button>
-
-          <button 
-            @click="salvarEdicao"
-            class="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 transition"
-          >
-            Salvar
-          </button>
-        </div>  
-      </div>
     </div>
 </template>
 
