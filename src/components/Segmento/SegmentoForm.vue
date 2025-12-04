b587c6f32ee0c393f860cca81004b67fdf00e612
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-04 16:29:33 -0400

    script de SegmentoForm

---
+++ src/components/Segmento/SegmentoForm.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { SegmentoService } from '@/services/SegmentoService'
+
+const descricao = ref('')
+
+const emit = defineEmits<{
+  (e: 'close'): void
+  (e: 'save'): void
+}>()
+
+async function salvar() {
+  try {
+    await SegmentoService.salvar({ descricao: descricao.value })
+    emit('save')
+    descricao.value = ''
+    fechar()
+  } catch (error) {
+    console.error('Erro ao salvar segmento:', error)
+    alert('Erro ao salvar segmento.')
+  }
+}
+
+function fechar(){
+  emit('close')
+}
+
+</script>
+
+<template></template>
+
+<style scoped></style>
