8f2b76b0f51592468ebc9c47f4e4d5ba103223a9
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-04 16:36:35 -0400

    script de SegmentoList

---
+++ src/components/Segmento/SegmentoList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps<{
+  segmentos: { uuid: string; descricao: string; ativo: boolean }[]
+}>()
+</script>
+
+<template></template>
+
+<style scoped></style>
