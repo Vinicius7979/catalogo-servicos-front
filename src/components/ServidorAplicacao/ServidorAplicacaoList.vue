9b1878c16cb657eb7088649bd20b19f6a6901b42
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-09 15:25:32 -0400

    script de ServidorAplicacaoList

---
+++ src/components/ServidorAplicacao/ServidorAplicacaoList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  servidoresAplicacao: { uuid: string;
+    descricao: string;
+    versao: string;
+    ip: string;
+    ambiente: string;
+    tipoServidor: string;
+    orquestrador: string;
+    url: string;
+    porta: number;
+    ipProxy: string;
+    hostName: string;
+    distribuicao: string;
+    ativo: boolean; }[]
+}>()
+</script>
+
+<template></template>
+
+<style scoped></style>
