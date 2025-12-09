66f95e023242ee9a9f686ccd901f8d58a39adfd8
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-09 14:17:03 -0400

    criação de DeployView

---
+++ src/views/DeployView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type BarraLateral from '@/components/BarraLateral.vue';
+import type DeployPage from '@/components/Deploy/DeployPage.vue';
+import type DefaultLayout from '@/layouts/DefaultLayout.vue';
+
+</script>
+
+<template>
+    <DefaultLayout>
+        <div class="flex min-h-[calc(100vh-120px)]">
+            <BarraLateral />
+            <DeployPage />
+        </div>
+    </DefaultLayout>
+</template>
+
+<style scoped></style>
