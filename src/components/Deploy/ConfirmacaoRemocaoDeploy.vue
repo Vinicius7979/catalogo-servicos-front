991613c3e3bbabac14022932207f744443f6dc28
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-09 14:18:54 -0400

    script de ConfirmacaoRemocaoDeploy

---
+++ src/components/Deploy/ConfirmacaoRemocaoDeploy.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+const emit = defineEmits<{
+    (e: 'cancel'): void
+    (e: 'confirm'): void
+}>()
+</script>
+
+<template></template>
+
+<style scoped></style>
