009938ec4ad1f420293f1589581cbec789ecf5fc
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-12-09 14:19:53 -0400

    template de ConfirmacaoRemocaoDeploy

--- src/components/Deploy/ConfirmacaoRemocaoDeploy.vue
+++ src/components/Deploy/ConfirmacaoRemocaoDeploy.vue
@@ -7,6 +7,28 @@ const emit = defineEmits<{
 }>()
 </script>
 
-<template></template>
+<template>
+    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div class="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-sm text-center">
+            <h2 class="text-lg font-semibold text-gray-800 mb-4">Deseja remover esse Deploy?</h2>
+        
+            <div class="flex justify-center gap-4">
+                <button
+                    @click="emit('cancel')"
+                    class="px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-400 transition"
+                >
+                    Cancelar
+                </button>
+
+                <button
+                    @click="emit('confirm')"
+                    class="px-4 py-2 rounded-lg bg-red-600 text-white hover:bg-red-700 transition"
+                >
+                    Confirmar
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
 
 <style scoped></style>
